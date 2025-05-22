ac39e932a09ddcf387c2738e4041d60188812025
skihyeon1001 <hero981001@gmail.com>
2025-05-22 06:37:29 +0000

    도커 컴포즈 파일 작성. ELASTICSEARCH_HOSTS를 프로퍼티로 정의하여 ES_HOST를 반환하도록 설정. (pydantic json parsing error)

--- app/core/config.py
+++ app/core/config.py
@@ -1,14 +1,19 @@
 from pydantic_settings import BaseSettings
 from pydantic.config import ConfigDict
 from typing import List
+import os
 
 class Settings(BaseSettings):
     APP_NAME: str = "Food Nutrition API"
     APP_VERSION: str = "0.0.1"
     APP_DESCRIPTION: str = "API for food nutrition data"
     
-    DATABASE_URL: str = "sqlite:///./food_nutrition_api.db"
-    ELASTICSEARCH_HOSTS: List[str] = ["http://localhost:9200"]
+    DATABASE_URL: str = os.getenv("DATABASE_URL", "sqlite:///./food_nutrition_api.db")
+    ES_HOST: str = os.getenv("ES_HOST", "http://localhost:9200")
+    
+    @property
+    def ELASTICSEARCH_HOSTS(self) -> List[str]:
+        return [self.ES_HOST]
 
 settings = Settings()
     
